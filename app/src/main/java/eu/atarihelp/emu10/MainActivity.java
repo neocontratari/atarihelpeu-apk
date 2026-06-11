9f067d5ff21c7a9bb1e0eeb9bba2de4c929edec4
neocontratari <neocontr1975@gmail.com>
2026-06-11 10:57:17 +0200

    AtariHelp_eu_EMU10_BUILD2_loader

--- app/src/main/java/eu/atarihelp/emu10/MainActivity.java
+++ app/src/main/java/eu/atarihelp/emu10/MainActivity.java
@@ -2,17 +2,22 @@ package eu.atarihelp.emu10;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 /**
- * AtariHelp.eu EMU-10 BUILD1 - tenky obal:
- * cely emulator (6502, ANTIC, GTIA, POKEY) bezi v JavaScriptu v assets/index.html.
+ * AtariHelp.eu EMU-10 BUILD2 - obal s podporou vyberu souboru
+ * (NAHRAJ XEX / NAHRAJ ATR potrebuje onShowFileChooser).
  */
 public class MainActivity extends Activity {
+    private static final int PICK_FILE = 1;
     private WebView web;
+    private ValueCallback<Uri[]> pendingChooser;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -23,10 +28,33 @@ public class MainActivity extends Activity {
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
         s.setAllowFileAccess(true);
-        s.setMediaPlaybackRequiresUserGesture(false); // POKEY zvuk hned po POWER
-        web.setWebChromeClient(new WebChromeClient());
+        s.setMediaPlaybackRequiresUserGesture(false);
+        web.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView v, ValueCallback<Uri[]> cb,
+                                             FileChooserParams params) {
+                if (pendingChooser != null) pendingChooser.onReceiveValue(null);
+                pendingChooser = cb;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                startActivityForResult(Intent.createChooser(i, "Vyber XEX / ATR"), PICK_FILE);
+                return true;
+            }
+        });
         web.loadUrl("file:///android_asset/index.html");
         setContentView(web);
+    }
+
+    @Override
+    protected void onActivityResult(int req, int res, Intent data) {
+        super.onActivityResult(req, res, data);
+        if (req == PICK_FILE && pendingChooser != null) {
+            Uri[] out = (res == RESULT_OK && data != null && data.getData() != null)
+                    ? new Uri[]{ data.getData() } : null;
+            pendingChooser.onReceiveValue(out);
+            pendingChooser = null;
+        }
     }
 
     @Override
