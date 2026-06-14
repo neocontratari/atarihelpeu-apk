63b5c6886dfa026926ae5f433a54891ba40f766a
neocontratari <neocontr1975@gmail.com>
2026-06-14 21:43:17 +0200

    AtariHelp_eu_EMU10_BUILD2AJ_XC12_REEL_COUNTER_ALIGN

--- app/src/main/java/eu/atarihelp/emu10/MainActivity.java
+++ app/src/main/java/eu/atarihelp/emu10/MainActivity.java
@@ -26,12 +26,12 @@ import java.net.URL;
 import java.net.URLDecoder;
 
 /**
- * AtariHelp.eu EMU-10 BUILD2AI
+ * AtariHelp.eu EMU-10 BUILD2AJ
  * - file chooser (NAHRAJ XEX/ATR/ZIP)
  * - AHSAVE (ulozeni logu)
  * - DownloadListener: ZIP/XEX/ATR z webu se stahne a rovnou spusti v emulatoru
  * - BUILD2AG UI: NET HRY + XC12 WAV/MP3 real seek pres REW/F.FWD
- * - BUILD2AI uvodni kazetak: samostatny MP3 prehravac hudby s realnym seekem
+ * - BUILD2AJ uvodni kazetak: samostatny MP3 prehravac; emu XC12 cívky/counter zarovnane
  */
 public class MainActivity extends Activity {
     private static final int PICK_FILE = 1;
