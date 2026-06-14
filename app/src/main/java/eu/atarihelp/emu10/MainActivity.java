b33b65ecceac54538d3f73c1a23f706ab390a3d5
neocontratari <neocontr1975@gmail.com>
2026-06-14 14:50:11 +0200

    AtariHelp_eu_EMU10_BUILD2T_EXACT_UI_CODE

--- app/src/main/java/eu/atarihelp/emu10/MainActivity.java
+++ app/src/main/java/eu/atarihelp/emu10/MainActivity.java
@@ -22,7 +22,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
- * AtariHelp.eu EMU-10 BUILD2S SAFE UI SKIN
+ * AtariHelp.eu EMU-10 BUILD2F
  * - file chooser (NAHRAJ XEX/ATR/ZIP)
  * - AHSAVE (ulozeni logu)
  * - DownloadListener: ZIP/XEX/ATR z webu se stahne a rovnou spusti v emulatoru
