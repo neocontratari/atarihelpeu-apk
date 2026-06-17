7d2c87bd08e85c48cb0c84ac27913676b426567a
neocontratari <neocontr1975@gmail.com>
2026-06-17 08:46:50 +0200

    AtariHelp_eu_EMU10_BUILD2DF_DB_SOUND_LOCK_NO_FAKE_REVERT

---
+++ build2df_check.js
@@ -0,0 +1,4 @@
+/* AtariHelp.eu EMU-10 BUILD2DF_DB_SOUND_LOCK_NO_FAKE_REVERT
+   Full overlay. Zalozeno na BUILD2DB zvukovem smeru, ne na DD/DE partial.
+   KODY JSOU STEJNE. TXT/joystick/scroll/focus nemeneno.
+*/
